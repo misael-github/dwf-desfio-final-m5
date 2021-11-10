9baf60e329e3c0a878a408880a285f7028751512
Misael <dmisael2000@gmail.com>
2021-11-10 11:12:52 -0300

    index.ts components change of ruote

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ import "./components/buttons";
 import "./components/text";
 import "./components/star";
 import "./components/countdown";
-//hands
-import "./../src/components/hands";
-import "./../src/components/hands/user-hands";
-import "./../src/components/hands/computer-hands";
+
+import "./components/hands";
+import "./components/hands/user-hands";
+import "./components/hands/computer-hands";
 
 (function () {
     state.init();
